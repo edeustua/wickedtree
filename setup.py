8c81fc7141c91b9d0efa85be77003776a4f26128
Emiliano Deustua <edeustua@gmail.com>
2020-07-10 12:48:11 -0400

    Created module structed

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wickedtree',
+      version='0.1',
+      description="Implementation of Wick's theorem using a binary tree",
+      author='J. Emiliano Deustua',
+      author_email='edeustua@gmail.com',
+      packages=['wickedtree'],
+     )
+
