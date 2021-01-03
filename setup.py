82fe10f354f54bdfcc2d8a38dd68e1e913da58ec
J. Emiliano Deustua <edeustua@gmail.com>
2021-01-03 13:33:58 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from distutils.core import setup
-
-setup(name='wickedtree',
-      version='0.1',
-      description="Implementation of Wick's theorem using a binary tree",
-      author='J. Emiliano Deustua',
-      author_email='edeustua@gmail.com',
-      install_requires=[
-          'click',
-      ],
-      extras_require={
-          'dev': [
-              'pytest',
-              'pytest-pep8',
-              'pytest-cov'
-          ]
-      },
-      packages=['wickedtree'],
+setup(
+    name='wickedtree',
+    version='0.1',
+    description="Implementation of Wick's theorem using a binary tree",
+    author='J. Emiliano Deustua',
+    author_email='edeustua@gmail.com',
+    install_requires=[
+        'click',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov'
+        ]
+    },
+    packages=find_packages(),
 )
 
